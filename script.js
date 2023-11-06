e004ce09978163e6db0fccd92f7bd4155fab2e39
Cj Alexander <clauvinsalexander@gmail.com>
2023-11-06 13:28:29 -0500

    Added drawing functionality with listeners

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".container")
 
-
+// For some reason it was printing an extra square
 let amountOfSquares = 16**2 -1
 
 for (let count = 0; count !== amountOfSquares; count++) {
@@ -8,3 +8,14 @@ for (let count = 0; count !== amountOfSquares; count++) {
     square.setAttribute('class', 'square')
     container.appendChild(square)
 }
+
+const highlightSquare = document.getElementsByClassName('square')
+// Turned it into an array so I can use forEach
+const eachSquare = [...highlightSquare].forEach((element) => {
+    // Chose to change the class whenever the mouse enters a square
+    element.addEventListener('mouseenter', (e) => {
+        e.target.setAttribute('class', "color-square")
+    })
+})
+
+
